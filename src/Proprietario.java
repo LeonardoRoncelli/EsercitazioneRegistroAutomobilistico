f89a23493e73b7b3b1f11ad1d4e2393a499ca675
RONCELLI.23346 <roncelli.leonardo.studente@itispaleocapa.it>
2025-05-19 12:51:05 +0200

    Attributi, metodi set e get e metodo toString della classe Targa.

--- src/Proprietario.java
+++ src/Proprietario.java
@@ -1,2 +1,33 @@
 public class Proprietario {
+    private String nome;
+    private String cognome;
+    private String codiceFiscale;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getCognome() {
+        return cognome;
+    }
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+    public String getCodiceFiscale() {
+        return codiceFiscale;
+    }
+    public void setCodiceFiscale(String codiceFiscale) {
+        this.codiceFiscale = codiceFiscale;
+    }
+    public Proprietario(String nome, String cognome, String codiceFiscale){
+        this.nome=nome;
+        this.cognome=cognome;
+        this.codiceFiscale=codiceFiscale;
+    }
+    public String toString(){
+        return "Nome: "+nome+"\n"+
+                "Cognome: "+cognome+"\n"+
+                "Codice fiscale: "+codiceFiscale;
+    }
 }
